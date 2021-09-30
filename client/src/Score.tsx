4899e7363d6c9e4da443f1f0cde45261c6775c8d
devashish-acharya <thedev96@gmail.com>
2021-09-30 16:31:39 +0100

    percentage, pass/fail

--- client/src/Score.tsx
+++ client/src/Score.tsx
@@ -14,9 +14,31 @@ export default function Score({ correctAnswers, dataAnswers }: props) {
     }
   }
 
-  return (
-    <Alert severity="error">
-      {score} / {correctAnswers.length}
-    </Alert>
-  );
+  
+//calculating percentage
+  function percentage(partialValue: number, totalValue: number) {
+    return (100 * partialValue) / totalValue;
+  } 
+  
+  const totalQuestions = correctAnswers.length;
+  const correctAns = score
+  const passPercentage = 65
+  
+  console.log(percentage(correctAns, totalQuestions))
+  
+  //determining pass or fail
+  if (percentage(correctAns, totalQuestions) >= passPercentage) {
+    return (
+      <Alert severity="success">{score} / {correctAnswers.length}            Percentage = {Math.round(percentage(correctAns, totalQuestions))}%  Pass</Alert>
+      
+    )}
+     else {
+       return (
+        <Alert severity="error">{score} / {correctAnswers.length}             Percentage = {Math.round(percentage(correctAns, totalQuestions))}% Better Luck Next Time</Alert>
+       )
+     }
+ 
+  
 }
+
+
