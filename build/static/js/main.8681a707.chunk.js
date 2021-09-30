56232b2607773b20884e5309cf7c2a26ab92e023
Vavom <43526045+Vavom@users.noreply.github.com>
2021-09-30 09:53:46 +0100

    Initial commit

---
+++ build/static/js/main.8681a707.chunk.js
@@ -0,0 +1,2 @@
+(this["webpackJsonpcreate-react-app"]=this["webpackJsonpcreate-react-app"]||[]).push([[0],{21:function(t,e,a){"use strict";a.r(e);a(0);var n=a(10),r=a.n(n),c=a(31),s=a(1);function p(){return Object(s.jsx)(c.a,{variant:"contained",children:"Start Test"})}r.a.render(Object(s.jsx)(p,{}),document.getElementById("root"))}},[[21,1,2]]]);
+//# sourceMappingURL=main.8681a707.chunk.js.map
