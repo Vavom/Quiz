5b78fb194a244bbc84b142475f5f90a1eaee6860
jsafrany <j.c.safrany@gmail.com>
2021-09-30 14:10:01 +0100

    api completed with tests

---
+++ api/startServer.js
@@ -0,0 +1,5 @@
+const {app,sequelize} = require('./server.js')
+
+app.listen(9000, () => {
+    sequelize.sync().then(() => console.log("ready to send out some of those dank, dank high scores baby!!"))
+})
