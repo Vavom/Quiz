ad78e10c65b0e020a1f22f2d6945e43d48aef84c
jsafrany <j.c.safrany@gmail.com>
2021-10-01 15:41:45 +0100

    removed direct port call

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 const {app,sequelize} = require('./api/server.js')
 const express = require('express');
 const path = require('path');
+const PORT = process.env.PORT || 9000
 
 app.use(express.static(path.join(__dirname,'client', 'build')));
 
@@ -8,6 +9,6 @@ app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'client', 'build', 'index.html'));
 });
 
-app.listen(9000, () => {
+app.listen(PORT, () => {
     sequelize.sync().then(() => console.log("ready to send out some of those dank, dank high scores baby!!"))
 })
