101a1be4fe355644be8e6e9a4431728c7f097dac
Vavom <43526045+Vavom@users.noreply.github.com>
2021-10-01 14:31:21 +0100

    Score page fixes

--- client/src/Score.tsx
+++ client/src/Score.tsx
@@ -9,11 +9,6 @@ type props = {
 };
 
 export default function Score({ correctAnswers, dataAnswers, time }: props) {
-  const [username, setUsername] = React.useState<string>("");
-  const [isAcceptableUsername, setIsAcceptableUsername] =
-    React.useState<boolean>(true);
-  const [helperText, setHelperText] = React.useState<string>("");
-  const [hasSumbitted, setHasSubmitted] = React.useState<boolean>(false);
   const [postFailed, setPostFailed] = React.useState(false);
 
   let score = 0;
@@ -23,28 +18,6 @@ export default function Score({ correctAnswers, dataAnswers, time }: props) {
       score++;
     }
   }
-
-  const handleUsernameChange = (event: React.ChangeEvent<HTMLInputElement>) => {
-    setUsername(event.target.value);
-    setIsAcceptableUsername(true);
-    setHelperText("");
-  };
-  const handleSubmit = () => {
-    if (username.length == 0) {
-      setIsAcceptableUsername(false);
-      setHelperText("Username must exist");
-      return;
-    }
-    const sendData = { username, score, time };
-    setHasSubmitted(true);
-    fetch("http://localhost:9000/scores", {
-      method: "POST",
-      headers: {
-        "Content-Type": "application/json",
-      },
-      body: JSON.stringify(sendData),
-    }).catch((err) => setPostFailed(true));
-  };
 
   //calculating percentage
   function percentage(partialValue: number, totalValue: number) {
@@ -58,65 +31,43 @@ export default function Score({ correctAnswers, dataAnswers, time }: props) {
   const submitScore = () => {
     return (
       <>
-        <Scoreboard score={score} />;
-        {!hasSumbitted && (
-          <>
-            <FormLabel component="legend">
-              Would you like to submit your score?
-            </FormLabel>
-            <TextField
-              id="username"
-              label="Name"
-              value={username}
-              onChange={handleUsernameChange}
-              error={!isAcceptableUsername}
-              helperText={helperText}
-            />
-            <Button
-              sx={{ mt: 1, mr: 1 }}
-              onClick={handleSubmit}
-              variant="outlined"
-              id="cy-button"
-            >
-              Submit
-            </Button>
-          </>
-        )}
+        <Scoreboard
+          score={score}
+          time={time}
+          setPostFailed={setPostFailed}
+          correctAnswers={correctAnswers}
+        />
+        ;
       </>
     );
   };
 
-  console.log(percentage(correctAns, totalQuestions));
-  const alert = () => {
-    if (!postFailed) {
-      if (percentage(correctAns, totalQuestions) >= passPercentage) {
-        return (
-          <>
-            <Alert severity="success">
-              {score} / {correctAnswers.length} Percentage ={" "}
-              {Math.round(percentage(correctAns, totalQuestions))}% Pass
-            </Alert>
-          </>
-        );
-      } else {
-        return (
-          <>
-            <Alert severity="error">
-              {score} / {correctAnswers.length} Percentage ={" "}
-              {Math.round(percentage(correctAns, totalQuestions))}% Better Luck
-              Next Time
-            </Alert>
-          </>
-        );
-      }
-    } else {
-      return <Alert severity="error">Cannot submit data!!</Alert>;
-    }
-  };
-  return (
-    <>
-      {alert()}
-      {!postFailed && submitScore()}
-    </>
-  );
+  // console.log(percentage(correctAns, totalQuestions));
+  // const alert = () => {
+  //   if (!postFailed) {
+  //     if (percentage(correctAns, totalQuestions) >= passPercentage) {
+  //       return (
+  //         <>
+  //           <Alert severity="success">
+  //             {score} / {correctAnswers.length} Percentage ={" "}
+  //             {Math.round(percentage(correctAns, totalQuestions))}% Pass
+  //           </Alert>
+  //         </>
+  //       );
+  //     } else {
+  //       return (
+  //         <>
+  //           <Alert severity="error">
+  //             {score} / {correctAnswers.length} Percentage ={" "}
+  //             {Math.round(percentage(correctAns, totalQuestions))}% Better Luck
+  //             Next Time
+  //           </Alert>
+  //         </>
+  //       );
+  //     }
+  //   } else {
+  //     return <Alert severity="error">Cannot submit data!!</Alert>;
+  //   }
+  // };
+  return <>{!postFailed && submitScore()}</>;
 }
--- client/src/Scoreboard.tsx
+++ client/src/Scoreboard.tsx
@@ -6,7 +6,13 @@ import TableContainer from "@mui/material/TableContainer";
 import TableHead from "@mui/material/TableHead";
 import TableRow from "@mui/material/TableRow";
 import Paper from "@mui/material/Paper";
-import { Alert, TablePagination } from "@mui/material";
+import {
+  Alert,
+  Button,
+  FormLabel,
+  TablePagination,
+  TextField,
+} from "@mui/material";
 import CustomPaginationActionsTable from "./CustomPaginationActionsTable";
 import {
   Card,
@@ -17,9 +23,13 @@ import {
   Box,
   Typography,
 } from "@mui/material";
+import zIndex from "@mui/material/styles/zIndex";
 
 type props = {
   score: number;
+  time: number;
+  setPostFailed: React.Dispatch<React.SetStateAction<boolean>>;
+  correctAnswers: (string | null)[];
 };
 
 type row = {
@@ -30,9 +40,43 @@ type row = {
 
 export type rows = Array<row>;
 
-export default function Scoreboard({ score }: props) {
+export default function Scoreboard({
+  score,
+  time,
+  setPostFailed,
+  correctAnswers,
+}: props) {
   const [rows, setRows] = React.useState<Array<row>>([]);
   const [loadingFailed, setLoadingFailed] = React.useState(false);
+  const [hasSumbitted, setHasSubmitted] = React.useState<boolean>(false);
+  const [isAcceptableUsername, setIsAcceptableUsername] =
+    React.useState<boolean>(true);
+  const [username, setUsername] = React.useState<string>("");
+  const [helperText, setHelperText] = React.useState<string>("");
+
+  const handleUsernameChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    setUsername(event.target.value);
+    setIsAcceptableUsername(true);
+    setHelperText("");
+  };
+
+  const handleSubmit = () => {
+    if (username.length == 0) {
+      setIsAcceptableUsername(false);
+      setHelperText("Username must exist");
+      return;
+    }
+    const sendData = { username, score, time };
+    setHasSubmitted(true);
+    fetch("http://localhost:9000/scores", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(sendData),
+    }).catch((err) => setPostFailed(true));
+  };
+
   const getData = async () => {
     fetch("http://localhost:9000/scores")
       .then((res) => res.json())
@@ -43,10 +87,19 @@ export default function Scoreboard({ score }: props) {
   };
   getData();
 
+  function percentage(partialValue: number, totalValue: number) {
+    return (100 * partialValue) / totalValue;
+  }
+
   if (rows !== null || !loadingFailed) {
     return (
       <Container
-        sx={{ height: "100vh", minWidth: "100%", bgcolor: "primary.main" }}
+        sx={{
+          height: "100vh",
+          minWidth: "100%",
+          bgcolor: "primary.main",
+          zIndex: "-1",
+        }}
       >
         <Box
           sx={{
@@ -55,6 +108,7 @@ export default function Scoreboard({ score }: props) {
             flexDirection: "row",
             justifyContent: "center",
             paddingTop: "20px",
+            paddingBottom: "40px",
           }}
         >
           <Box
@@ -64,7 +118,16 @@ export default function Scoreboard({ score }: props) {
               justifyContent: "center",
             }}
           >
-            <Typography variant="h1">Highscores!</Typography>
+            <Typography
+              sx={{
+                display: "flex",
+                flexDirection: "row",
+                justifyContent: "center",
+              }}
+              variant="h1"
+            >
+              Highscores!
+            </Typography>
             <Typography
               variant="h4"
               sx={{
@@ -74,26 +137,49 @@ export default function Scoreboard({ score }: props) {
                 paddingTop: "20px",
               }}
             >
-              {"Your current score: " + score}
+              {score} / {correctAnswers.length} Percentage ={" "}
+              {Math.round(percentage(score, correctAnswers.length))}%{" "}
+              {Math.round(percentage(score, correctAnswers.length)) >= 65
+                ? "Pass"
+                : "Better Luck Next Time"}
             </Typography>
+            {!hasSumbitted && (
+              <>
+                <FormLabel component="legend">
+                  Would you like to submit your score?
+                </FormLabel>
+                <TextField
+                  id="username"
+                  label="Name"
+                  value={username}
+                  onChange={handleUsernameChange}
+                  error={!isAcceptableUsername}
+                  helperText={helperText}
+                />
+                <Button
+                  sx={{
+                    mt: 1,
+                    mr: 1,
+                    bgcolor: "secondary.main",
+                    display: "flex",
+                    flexDirection: "row",
+                    alignSelf: "center",
+                  }}
+                  onClick={handleSubmit}
+                  variant="outlined"
+                  id="cy-button"
+                >
+                  Submit
+                </Button>
+              </>
+            )}
+            <Card>
+              <Paper sx={{ width: "100%", overflow: "hidden" }}>
+                <CustomPaginationActionsTable rows={rows} />
+              </Paper>
+            </Card>
           </Box>
         </Box>
-        <Card
-          sx={{
-            maxWidth: "50%",
-            borderRadius: "16px",
-            left: "25%",
-            top: "25%",
-            right: "25%",
-            bottom: "10%",
-            position: "absolute",
-            p: 1,
-          }}
-        >
-          <Paper sx={{ width: "100%", overflow: "hidden" }}>
-            <CustomPaginationActionsTable rows={rows} />
-          </Paper>
-        </Card>
       </Container>
     );
   } else {
