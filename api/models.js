db1f2f575e380a691dff41faa4fd22a724a3f611
jsafrany <j.c.safrany@gmail.com>
2021-10-01 15:47:50 +0100

    added pg for production

--- api/models.js
+++ api/models.js
@@ -3,9 +3,10 @@ const path = require('path')
 
 const connectionSettings = {
     test: {dialect: 'sqlite', storage: ':memory:',logging: false},
-    prodAndDev: {dialect: 'sqlite', storage: path.join(__dirname, 'data.db')},
+    dev: {dialect: 'sqlite', storage: path.join(__dirname, 'data.db')},
+    production: {dialect: 'postgres', protocal: 'postgres'},
 }
-const sequelize =  new Sequelize(process.env.NODE_ENV == 'test' ? connectionSettings['test'] : connectionSettings['prodAndDev'])
+const sequelize =  new Sequelize(connectionSettings['process.env.NODE_ENV'])
 
 class Scores extends Model {}
 
